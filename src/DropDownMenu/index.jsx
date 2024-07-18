60a902cd9dc3967e766412bb87387b46bf662e08
doigaki mikuru <jm.jo.zeppelin0702@mineo.jp>
2024-07-18 23:49:03 +0900

    feat: test2

--- src/DropDownMenu/index.jsx
+++ src/DropDownMenu/index.jsx
@@ -1,5 +1,19 @@
 import styles from "./index.module.scss";
 
 export const DropDownMenu = () => {
-  return <></>;
+  return (
+    <>
+      <div className={styles.dropDownMenu}>
+        <h1 className={styles.dropDownMenuLogo}>TANAKA Works</h1>
+        <button className={styles.dropDownMenuButton}>x</button>
+        <ul className={styles.dropDownMenuList}>
+          <li className="dropDownMenuitem">会社概要</li>
+          <li className="dropDownMenuitem">価格表</li>
+          <li className="dropDownMenuitem">実績</li>
+          <li className="dropDownMenuitem">アクセス</li>
+          <li className="dropDownMenuitem">よくある質問</li>
+        </ul>
+      </div>
+    </>
+  );
 };
