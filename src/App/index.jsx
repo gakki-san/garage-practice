1b2303c71316650439bfce965b62cf161ae35078
doigaki mikuru <jm.jo.zeppelin0702@mineo.jp>
2024-07-18 23:46:56 +0900

    feat: テスト

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,10 +1,12 @@
 import { Button } from "../Button/index.jsx";
+import { DropDownMenu } from "../DropDownMenu/index.jsx";
 import styles from "./index.module.scss";
 
 function App() {
   return (
     <div>
       <Button />
+      <DropDownMenu />
     </div>
   );
 }
