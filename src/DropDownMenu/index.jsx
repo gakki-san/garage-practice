f183749a7a687c6439c81425dc35f69f7e7b7625
doigaki mikuru <jm.jo.zeppelin0702@mineo.jp>
2024-07-19 00:13:38 +0900

    feat: ドロップダウンメニューの文字の装飾をしました。

--- src/DropDownMenu/index.jsx
+++ src/DropDownMenu/index.jsx
@@ -4,8 +4,10 @@ export const DropDownMenu = () => {
   return (
     <>
       <div className={styles.dropDownMenu}>
-        <h1 className={styles.dropDownMenuLogo}>TANAKA Works</h1>
-        <button className={styles.dropDownMenuButton}>x</button>
+        <div className={styles.dropDownMenuTop}>
+          <h1 className={styles.dropDownMenuLogo}>TANAKAWorks</h1>
+          <button className={styles.dropDownMenuButton}>x</button>
+        </div>
         <ul className={styles.dropDownMenuList}>
           <li className="dropDownMenuitem">会社概要</li>
           <li className="dropDownMenuitem">価格表</li>
