9c183810edc00cf8575543156ed87dbda0578f96
doigaki mikuru <jm.jo.zeppelin0702@mineo.jp>
2024-07-24 18:26:21 +0900

    feat:headのhtml,cssの追加を行いました

--- src/Head/index.jsx
+++ src/Head/index.jsx
@@ -1,44 +1,48 @@
 import { Hamburger } from "../Hamburger";
+import { HeaderNav } from "../HeaderNav";
 import styles from "./index.module.scss";
 
 export const Head = () => {
   return (
     <>
       <div className={styles.header}>
-        <div className={styles.headerTop}>
-          <div className={styles.headerLeft}>
-            <div className={styles.headerHamburgerWrapper}>
-              <button className={styles.headerHamburger}>
-                <Hamburger />
-              </button>
+        <div className={styles.headerPadding}>
+          <div className={styles.headerTop}>
+            <div className={styles.headerLeft}>
+              <div className={styles.headerHamburgerWrapper}>
+                <button className={styles.headerHamburger}>
+                  <Hamburger />
+                </button>
+              </div>
+              <div className={styles.headerLogoWrapper}>
+                <button className={styles.headerLogo}>
+                  <img src="" alt="" />◎
+                </button>
+              </div>
+              <div className={styles.headerName}>trptz</div>
             </div>
-            <div className={styles.headerLogoWrapper}>
-              <button className={styles.headerLogo}>
-                <img src="" alt="" />◎
-              </button>
+            <div className={styles.headerRight}>
+              <input
+                type="text"
+                placeholder="type to search"
+                className={styles.headerInput}></input>
+              <div className={styles.headerRightButtonWrapper}>
+                <button className={styles.headerButton}>○</button>
+              </div>
+              <div className={styles.headerRightButtonWrapper}>
+                <button className={styles.headerButton}>○</button>
+              </div>
+              <div className={styles.headerRightButtonWrapper}>
+                <button className={styles.headerButton}>○</button>
+              </div>
+              <div className={styles.headerRightIconWrapper}>
+                <button className={styles.headerButton}>
+                  <img src="" alt="" />◎
+                </button>
+              </div>
             </div>
-            <div className={styles.headerName}>trptz</div>
           </div>
-          <div className={styles.headerRight}>
-            <input
-              type="text"
-              placeholder="type to search"
-              className={styles.headerInput}></input>
-            <div className={styles.headerRightButtonWrapper}>
-              <button className={styles.headerButton}>○</button>
-            </div>
-            <div className={styles.headerRightButtonWrapper}>
-              <button className={styles.headerButton}>○</button>
-            </div>
-            <div className={styles.headerRightButtonWrapper}>
-              <button className={styles.headerButton}>○</button>
-            </div>
-            <div className={styles.headerRightIconWrapper}>
-              <button className={styles.headerButton}>
-                <img src="" alt="" />◎
-              </button>
-            </div>
-          </div>
+          <HeaderNav />
         </div>
       </div>
     </>
---
+++ src/HeaderNav/index.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import styles from "./index.module.scss";
+
+export const HeaderNav = () => {
+  const [selectedHeaderButton, setSelectedHeaderButton] = useState(0);
+
+  const buttons = ["Overview", "Repositories", "Projects", "packages", "Stars"];
+  return (
+    <>
+      <div className={styles.headerNav}>
+        <div className={styles.headerNavItems}>
+          {buttons.map((text, index) => (
+            <button
+              key={index}
+              className={`${styles.headerNavButton} ${selectedHeaderButton === index ? styles.headerSelected : ""}`}
+              onClick={() => setSelectedHeaderButton(index)}>
+              <div className={styles.headerNavIcon}>◎</div>
+              <p className={styles.headerNavText}>{text}</p>
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
