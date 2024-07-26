c6ba1752dfcff3492a2a1f495206fdbf2edb7709
doigaki mikuru <jm.jo.zeppelin0702@mineo.jp>
2024-07-26 15:59:27 +0900

    feat: overviewの構築を実施

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,10 +1,12 @@
 import { Head } from "../Head";
+import { Overview } from "../Overview";
 import styles from "./index.module.scss";
 
 export const App = () => {
   return (
     <>
       <Head />
+      <Overview />
     </>
   );
 };
---
+++ src/Overview/index.jsx
@@ -0,0 +1,45 @@
+import { OverviewCard } from "../OverviewCard";
+import styles from "./index.module.scss";
+
+export const Overview = () => {
+  return (
+    <>
+      <div className={styles.overview}>
+        <div className={styles.overviewPadding}>
+          <div className={styles.overviewContainer}>
+            <div className={styles.overviewLeft}>
+              <div className={styles.overviewImageWrapper}>
+                <img
+                  className={styles.overviewImage}
+                  src="https://avatars.githubusercontent.com/u/40844605?v=4"
+                  alt="ごんたの写真"
+                />
+              </div>
+              <div className={styles.overviewLeftNameWrapper}>
+                <p className={styles.overviewLeftName}>trptz</p>
+                <p className={styles.overviewLeftNickName}>trptz</p>
+              </div>
+              <button className={styles.overviewLeftEditButton}>
+                Edit Profile
+              </button>
+            </div>
+            <div className={styles.overviewRight}>
+              <div className={styles.overviewRightTitleWrapper}>
+                <h3 className={styles.overviewRightTitle}>
+                  Popular Repositories
+                </h3>
+                <p className={styles.overviewRightText}>Customize your pins</p>
+              </div>
+              <div className={styles.overviewRightCards}>
+                <OverviewCard />
+                <OverviewCard />
+                <OverviewCard />
+                <OverviewCard />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
