8caf01139a0c487319fcd07417a5b12d14f32882
doigaki mikuru <jm.jo.zeppelin0702@mineo.jp>
2024-07-27 13:31:19 +0900

    feat: footerのHTMlCSSの構築をしました

--- src/footer/index.jsx
+++ src/footer/index.jsx
@@ -1,5 +1,39 @@
 import styles from "./index.module.scss";
 
 export const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <div className={styles.footer}>
+        <div className={styles.footerPadding}>
+          <div className={styles.footerContainer}>
+            <div className={styles.footerLogo}>
+              <img src="" alt="" />◎
+            </div>
+            <div className={styles.footerItem}>◎2024 Github,inc</div>
+            <div className={styles.footerItem}>
+              <a href="#">Term</a>
+            </div>
+            <div className={styles.footerItem}>
+              <a href="#">Privacy</a>
+            </div>
+            <div className={styles.footerItem}>
+              <a href="#">Security</a>
+            </div>
+            <div className={styles.footerItem}>
+              <a href="#">Status</a>
+            </div>
+            <div className={styles.footerItem}>
+              <a href="#">Contact</a>
+            </div>
+            <div className={styles.footerItem}>
+              <a href="#">Manga cooks</a>
+            </div>
+            <div className={styles.footerItem}>
+              <a href="#">Do not share my personal information</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
