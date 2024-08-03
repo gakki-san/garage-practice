e620c0b0d7b1ed1895f7f9ec9e53b2458bd0d340
doigaki mikuru <jm.jo.zeppelin0702@mineo.jp>
2024-08-03 13:19:29 +0900

    refacter: footerのクラス命名を変更しました。

--- src/footer/index.jsx
+++ src/footer/index.jsx
@@ -3,31 +3,31 @@ import styles from "./index.module.scss";
 export const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div className={styles.footerPadding}>
-        <div className={styles.footerContainer}>
-          <div className={styles.footerLogo}>
+      <div className={styles.sideSpacing}>
+        <div className={styles.container}>
+          <div className={styles.logo}>
             <img src="" alt="" />◎
           </div>
-          <div className={styles.footerTitle}>2024 Github,inc</div>
-          <div className={styles.footerItem}>
+          <div className={styles.title}>2024 Github,inc</div>
+          <div className={styles.linkItem}>
             <a href="#">Term</a>
           </div>
-          <div className={styles.footerItem}>
+          <div className={styles.linkItem}>
             <a href="#">Privacy</a>
           </div>
-          <div className={styles.footerItem}>
+          <div className={styles.linkItem}>
             <a href="#">Security</a>
           </div>
-          <div className={styles.footerItem}>
+          <div className={styles.linkItem}>
             <a href="#">Status</a>
           </div>
-          <div className={styles.footerItem}>
+          <div className={styles.linkItem}>
             <a href="#">Contact</a>
           </div>
-          <div className={styles.footerItem}>
+          <div className={styles.linkItem}>
             <a href="#">Manga cooks</a>
           </div>
-          <div className={styles.footerItem}>
+          <div className={styles.linkItem}>
             <a href="#">Do not share my personal information</a>
           </div>
         </div>
