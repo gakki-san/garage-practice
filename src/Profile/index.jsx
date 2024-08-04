21eb95f81ad8bd05dfdad181bb08eed36e282172
doigaki mikuru <jm.jo.zeppelin0702@mineo.jp>
2024-08-04 21:06:00 +0900

    refacter: overviewをprofileに命名変更しました。

--- src/Profile/index.jsx
+++ src/Profile/index.jsx
@@ -4,34 +4,32 @@ import styles from "./index.module.scss";
 export const Profile = () => {
   return (
     <div className={styles.profile}>
-      <div className={styles.profilePadding}>
+      <div className={styles.sideSpacing}>
         <div className={styles.profileContainer}>
-          <div className={styles.profileLeft}>
-            <div className={styles.profileSpLeft}>
-              <div className={styles.profileImageWrapper}>
+          <div className={styles.account}>
+            <div className={styles.spAccount}>
+              <div className={styles.imageWrapper}>
                 <img
-                  className={styles.profileImage}
+                  className={styles.image}
                   src="https://avatars.githubusercontent.com/u/40844605?v=4"
                   alt="ごんたの写真"
                 />
               </div>
-              <div className={styles.profileLeftNameWrapper}>
-                <p className={styles.profileLeftName}>trptz</p>
-                <p className={styles.profileLeftNickName}>trptz</p>
+              <div className={styles.nameWrapper}>
+                <p className={styles.accountName}>trptz</p>
+                <p className={styles.nickName}>trptz</p>
               </div>
             </div>
-            <div className={styles.profileLeftEditButtonWrapper}>
-              <button className={styles.profileLeftEditButton}>
-                Edit Profile
-              </button>
+            <div className={styles.buttonWrapper}>
+              <button className={styles.editButton}>Edit Profile</button>
             </div>
           </div>
-          <div className={styles.profileRight}>
-            <div className={styles.profileRightTitleWrapper}>
-              <h3 className={styles.profileRightTitle}>Popular Repositories</h3>
-              <p className={styles.profileRightText}>Customize your pins</p>
+          <div className={styles.repositoriesWrapper}>
+            <div className={styles.titleWrapper}>
+              <h3 className={styles.title}>Popular Repositories</h3>
+              <p className={styles.text}>Customize your pins</p>
             </div>
-            <div className={styles.profileRightCards}>
+            <div className={styles.repositoriesCard}>
               <ProfileCard />
               <ProfileCard />
               <ProfileCard />
