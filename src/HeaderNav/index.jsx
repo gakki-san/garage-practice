371d771fd97cc57594f7413bf3d397a38f2c64ea
doigaki mikuru <jm.jo.zeppelin0702@mineo.jp>
2024-08-05 19:15:30 +0900

    refacter: keyの中身をidにしました。また、?を&&に変えました。

--- src/HeaderNav/index.jsx
+++ src/HeaderNav/index.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import styles from "./index.module.scss";
-const buttons = ["Overview", "Repositories", "Projects", "packages", "Stars"];
+const buttons = [
+  { id: 1, text: "Overview" },
+  { id: 2, text: "Repositories" },
+  { id: 3, text: "Projects" },
+  { id: 4, text: "packages" },
+  { id: 5, text: "Stars" },
+];
 
 export const HeaderNav = () => {
   const [selectedHeaderButtonIndex, setSelectedHeaderButtonIndex] = useState(0);
@@ -8,13 +14,13 @@ export const HeaderNav = () => {
   return (
     <div className={styles.headerNav}>
       <div className={styles.NavItems}>
-        {buttons.map((text, index) => (
+        {buttons.map((button, index) => (
           <button
-            key={index}
-            className={`${styles.navButton} ${selectedHeaderButtonIndex === index ? styles.headerSelected : ""}`}
+            key={button.id}
+            className={`${styles.navButton} ${selectedHeaderButtonIndex === index && styles.headerSelected}`}
             onClick={() => setSelectedHeaderButtonIndex(index)}>
             <div className={styles.navIcon}>◎</div>
-            <p className={styles.navText}>{text}</p>
+            <p className={styles.navText}>{button.text}</p>
           </button>
         ))}
       </div>
