6b1c23f0b52c30cd2222593e09c7c1acab2e832d
doigaki mikuru <jm.jo.zeppelin0702@mineo.jp>
2024-08-07 16:07:40 +0900

    refacter: タグをdivからul liに変更しました

--- src/footer/index.jsx
+++ src/footer/index.jsx
@@ -1,5 +1,15 @@
 import styles from "./index.module.scss";
 
+const linkItems = [
+  { id: 1, text: "Term" },
+  { id: 2, text: "Privacy" },
+  { id: 3, text: "Security" },
+  { id: 4, text: "Status" },
+  { id: 5, text: "Contact" },
+  { id: 6, text: "Manga cooks" },
+  { id: 7, text: "Do not share my personal information" },
+];
+
 export const Footer = () => {
   return (
     <div className={styles.footer}>
@@ -9,27 +19,13 @@ export const Footer = () => {
             <img src="" alt="" />◎
           </div>
           <div className={styles.title}>2024 Github,inc</div>
-          <div className={styles.linkItem}>
-            <a href="#">Term</a>
-          </div>
-          <div className={styles.linkItem}>
-            <a href="#">Privacy</a>
-          </div>
-          <div className={styles.linkItem}>
-            <a href="#">Security</a>
-          </div>
-          <div className={styles.linkItem}>
-            <a href="#">Status</a>
-          </div>
-          <div className={styles.linkItem}>
-            <a href="#">Contact</a>
-          </div>
-          <div className={styles.linkItem}>
-            <a href="#">Manga cooks</a>
-          </div>
-          <div className={styles.linkItem}>
-            <a href="#">Do not share my personal information</a>
-          </div>
+          <ul className={styles.linkItemList}>
+            {linkItems.map((linkItem) => (
+              <li className={styles.linkItem} key={linkItem.id}>
+                <a href="#">{linkItem.text}</a>
+              </li>
+            ))}
+          </ul>
         </div>
         <div className={styles.footerSpItem}>◎2024 Github,inc</div>
       </div>
